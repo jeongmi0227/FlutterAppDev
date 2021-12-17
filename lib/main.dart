868dd042814d8592df1321f6a1047f2d80f59437
jeongmipark <parkjeongmi0227@gmail.com>
2021-12-16 20:10:26 -0500

    Creating a Flutter Project with MaterialApp

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// Author Name :  Jeong mi Park
+// Instructor : AngelaYu From Linkedin Learning
+// Modified : Dec 16, 2021.
+// Description : Flutter - Creating a Flutter project from scratch with Scaffolding a MaterialApp
+
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our flutter apps.
+void main() {
+  runApp(
+    // MaterialApp is generic top-level of the widget provided by flutter
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[300],
+        // AppBar widget display app bar with the Text message
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        // Center widget will align the items
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3gJ3z0avzNGkcjGitKnu-07YhUh1Hcd29OQ&usqp=CAU'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
